fc52bc31594ba7be169a239ed3b226f200341348
Pomegranate Daemon <nicholas.mikava@gmail.com>
2024-10-05 23:45:13 +0300

    add Spring integration test

---
+++ spring-anthill-integration-testing/src/main/java/com/pomegranated/anthill/testing/integration/spring/SpringAnthillIntegrationTestingApplication.java
@@ -0,0 +1,11 @@
+package com.pomegranated.anthill.testing.integration.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnthillIntegrationTestingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAnthillIntegrationTestingApplication.class, args);
+    }
+}
